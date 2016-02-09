7d157a494f637ef9f8ad6bc20081ea5584cb7e86
Quentin Buathier <quentin.buathier@epitech.eu>
2016-02-09 21:56:35 +0100

    Remove useless log

--- DirectX12FromScratch/src/MiniEngine/D3D12/D3D12ConstantBuffer.cpp
+++ DirectX12FromScratch/src/MiniEngine/D3D12/D3D12ConstantBuffer.cpp
@@ -205,8 +205,6 @@ bool D3D12ConstantBuffer::updateLights(std::list<Light*> &lights)
         Vector3f    position = light->getParent()->getDerivedPosition();
         Vector4f    direction = light->getParent()->getTransformationMatrix() * Vector4f(light->getDirection().x, light->getDirection().y, light->getDirection().z, 0.0f);
 
-        std::cout << direction << std::endl;
-
         lightsData[i].type = light->getType();
         lightsData[i].position = { position.x, position.y, position.z };
         lightsData[i].ambient = { light->getAmbient().x, light->getAmbient().y, light->getAmbient().z };
